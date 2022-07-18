0380e47d088ce577399d492f9544b930bc01f517
Nicolas Guinet <nguinet.pro@gmail.com>
2022-07-18 14:03:29 +0200

    Add version_config.js

---
+++ js/version_config.js
@@ -0,0 +1,9 @@
+export const current = "0.2.0";
+export const version_check_path = "https://raw.githubusercontent.com/guinetn/wed/main/js/version_config.js";
+export const app_url = "https://github.com/guinetn/wed";
+
+export const version_config = {
+	...current,
+	...version_check_path,
+	...app_url
+}
